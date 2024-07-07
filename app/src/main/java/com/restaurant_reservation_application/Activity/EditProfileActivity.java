ba1fdc9092f96c5e3633199a5564b651962e0bd2
Nguyễn Đức Anh <133002625+NguyenDucAnh1908@users.noreply.github.com>
2024-07-08 00:35:13 +0700

    DucAnh
    : Design edit profile

---
+++ app/src/main/java/com/restaurant_reservation_application/Activity/EditProfileActivity.java
@@ -0,0 +1,24 @@
+package com.restaurant_reservation_application.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.restaurant_reservation_application.R;
+import com.restaurant_reservation_application.databinding.ActivityEditProfileBinding;
+
+public class EditProfileActivity extends BaseActivity {
+    ActivityEditProfileBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityEditProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
